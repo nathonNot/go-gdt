e593384d2b649763fa4a9b7e497eb64e53c75ed9
liuyu <liuyu@ilingtuo.com>
2023-10-31 13:08:25 +0800

    增加log字段

--- config/db.go
+++ config/db.go
@@ -5,7 +5,7 @@ type DbConfig struct {
 }
 
 type RedisConfig struct {
-	RedisAddr string `json:"redis_addr"`
-	RedisPass string `json:"redis_pass"`
-	RedisUser string `json:"redis_user"`
+	RedisAddr []string `json:"redis_addr"`
+	RedisPass string   `json:"redis_pass"`
+	RedisUser string   `json:"redis_user"`
 }
--- log/wrapper.go
+++ log/wrapper.go
@@ -1,6 +1,43 @@
 package log
 
-import "github.com/sirupsen/logrus"
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+)
+
+// 埋点相关标记
+const (
+	isActionLog = "ActionLog"
+)
+
+// 服务器行为标记
+const (
+	ServerActionHourRefresh         = "ServerHourRefresh"
+	ServerActionDailyRefresh        = "ServerDailyRefresh"
+	ServerActionWeeklyRefresh       = "ServerActionWeeklyRefresh"
+	ServerActionPlayerConnectWithIp = "ServerActionPlayerConnectWithIp"
+)
+
+// 玩家相关的行为标记
+const (
+	PlayerLogId        = "PlayerId"
+	PlayerDbLogId      = "PlayerDbLogId"
+	ConnLogId          = "ConnId"
+	ApiLog             = "api"
+	ModuleLog          = "module"
+	RoomLogId          = "RoomId"
+	MsgTypeIdLog       = "MsgTypeId"
+	AddRoleScore       = "AddRoleScore"
+	ServerLogId        = "ServerId"
+	TeamLogId          = "TeamId"
+	ContainerIdLog     = "ContainerId"
+	AwsTaskLog         = "AwsTaskLog"
+	AwsEniLog          = "AwsEniLog"
+	DockerContainerLog = "DockerContainerLog"
+	ServerActionLog    = "ServerActionLog"
+	WeaponNftLog       = "WeaponNFTLog"
+	WeaponDbId         = "WeaponDbIdLog"
+)
 
 type Wrapper struct {
 	*logrus.Entry
@@ -10,4 +47,22 @@ var Logger *Wrapper
 
 func ServerLog() *Wrapper {
 	return Logger
+}
+func (l *Wrapper) ApiLog() *Wrapper {
+	return &Wrapper{l.WithFields(logrus.Fields{
+		ModuleLog: ApiLog,
+	})}
+}
+func (l *Wrapper) WithPlayerLog(playerId string) *Wrapper {
+	if playerId == "" {
+		return l
+	}
+	return &Wrapper{
+		Entry: l.WithField(PlayerLogId, playerId)}
+}
+
+func (l *Wrapper) WithPlayerDbLog(playerId uint) *Wrapper {
+	idStr := fmt.Sprintf("%d", playerId)
+	return &Wrapper{
+		Entry: l.WithField(PlayerDbLogId, idStr)}
 }
