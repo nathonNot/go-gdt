8b237a2fb8854dd8d878b1d38da32a1719e4bf4e
liuyu <liuyu@ilingtuo.com>
2023-11-06 17:27:30 +0800

    init放到game 层来做

--- igame/component.go
+++ igame/component.go
@@ -1,9 +1,5 @@
 package igame
 
-import (
-	"reflect"
-)
-
 type IComponent interface {
 	OnInit(ctx interface{})
 	OnLoad(ctx interface{})
@@ -16,26 +12,12 @@ type IComponent interface {
 var componentRoot ComponentRoot
 
 type ComponentRoot struct {
-	Components   map[string]IComponent
 	ComponentRef interface{}
 }
 
 func InitComponentRoot(root interface{}) {
 	componentRoot = ComponentRoot{}
-	componentRoot.Components = make(map[string]IComponent)
 	componentRoot.ComponentRef = root
-	val := reflect.Indirect(reflect.ValueOf(root))
-	for i := 0; i < val.NumField(); i++ {
-		fieldValue := val.Field(i)
-		// 判断字段类型是否为 combase
-		// 将字段名称赋值给 ComName 字段
-		if !fieldValue.CanInterface() {
-			continue
-		}
-		iCom := fieldValue.Interface().(IComponent)
-		componentRoot.Components[iCom.ComName()] = iCom
-	}
-
 }
 
 func NewComponents() ComponentRoot {
