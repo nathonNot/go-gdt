d6315e47ae6cfadb855a9bbb95738708c9a238a5
liuyu <liuyu@ilingtuo.com>
2023-11-08 17:38:44 +0800

    增加接口

--- igame/component.go
+++ igame/component.go
@@ -3,11 +3,13 @@ package igame
 type IComponent interface {
 	OnInit(ctx interface{})
 	OnLoad(ctx interface{})
-	OnSave(ctx interface{})
 	ComName() string
 	OnActive() error
 	OnDeActive() error
 	CustomizeSave() bool
+	CustomizeLoad() bool
+	Load(ctx interface{})
+	Save(ctx interface{})
 }
 
 var componentRoot ComponentRoot
