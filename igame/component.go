284120772c95f8fa44285a6c98a28c6e3287a69c
liuyu <liuyu@ilingtuo.com>
2023-11-09 14:26:57 +0800

    增加接口时序

--- igame/component.go
+++ igame/component.go
@@ -10,17 +10,30 @@ type IComponent interface {
 	CustomizeLoad() bool
 	Load(ctx interface{})
 	Save(ctx interface{})
+	ToJson() []byte
+	FromJson(data []byte)
 }
 
 var componentRoot ComponentRoot
 
 type ComponentRoot struct {
 	ComponentRef interface{}
+	ComponentMap map[string]IComponent
 }
 
 func InitComponentRoot(root interface{}) {
 	componentRoot = ComponentRoot{}
 	componentRoot.ComponentRef = root
+	componentRoot.ComponentMap = make(map[string]IComponent)
+	//val := reflect.Indirect(reflect.ValueOf(root))
+	//for i := 0; i < val.NumField(); i++ {
+	//	fieldValue := val.Field(i)
+	//	if !fieldValue.CanInterface() {
+	//		continue
+	//	}
+	//	iCom := fieldValue.Interface().(IComponent)
+	//	componentRoot.ComponentMap[iCom.ComName()] = iCom
+	//}
 }
 
 func NewComponents() ComponentRoot {
